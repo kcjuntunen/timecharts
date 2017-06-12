d07131d4864cd7b848e541c75b5e5715cf4be66f
K. C. Juntunen <kcjuntunen@frontier.com>
2017-06-12 06:38:40 -0400

    Return the last 100 log entries.

--- getLog.php
+++ getLog.php
@@ -5,26 +5,14 @@ $tz = 'UTC';
 date_default_timezone_set($tz);
 
 function returnRawLog($conn) {
-    $json = '';
     $output = array();
     $sql = "SELECT MACHINE, TS, EVENT FROM CUT_CYCLE_EVENTS ORDER BY ID DESC LIMIT 100";
     $res = $conn->query($sql);
     while ($a = $res->fetch_assoc()) {
         $output[] = $a;
     }
-    //echo var_dump($output[0]);
-    $json .= "[ ";
-    for ($i = 0; $i < count($output); $i++) {
-        $event = $output[$i]['EVENT'];
-        $event = str_replace('"', '', $event);
-        $json .= "{ \"timestamp\": \"{$output[$i]['TS']}\", \"machine\": \"{$output[$i]['MACHINE']}\", \"event\": \"$event\" },";
-    }
-
-    $json .= " ]";
-    $output = str_replace(', ]', ' ]', $json);
-    return $output;
+    return json_encode($output);
 }
-
 
 echo returnRawLog($mysqli);
 ?>
