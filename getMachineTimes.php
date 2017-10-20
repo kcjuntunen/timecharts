d90d78ced4343a95c03c0148f02dab8650940bff
K. C. Juntunen <kcjuntunen@frontier.com>
2017-10-20 09:19:07 -0400

    Refactored into proper functions.

--- getMachineTimes.php
+++ getMachineTimes.php
@@ -1,11 +1,7 @@
 <?php
-$config = parse_ini_file('/etc/cycles.conf');
-$mysqli = new mysqli($config['host'], $config['user'], $config['pass'], $config['db']);
 $tz = 'UTC';
 date_default_timezone_set($tz);
-$machine_list_data = $mysqli->query("SELECT DISTINCT MACHNUM FROM CUT_CYCLE_TIMES ORDER BY MACHNUM");
 
-$result = array();
 if (isset($_REQUEST["start"])) {
     setcookie("start", strtotime($_REQUEST["start"]), time() + 60 * 60 * 24 * 30);
 }
@@ -15,6 +11,19 @@ if (isset($_REQUEST["end"])) {
 }
 
 if (isset($_REQUEST['after'])) {
+    $config = parse_ini_file('/etc/cycles.conf');
+    $mysqli = new mysqli($config['host'], $config['user'], $config['pass'], $config['db']);
+    echo json_encode(getAfterID($mysqli));
+    $mysqli->close();
+} else {
+    $config = parse_ini_file('/etc/cycles.conf');
+    $mysqli = new mysqli($config['host'], $config['user'], $config['pass'], $config['db']);
+    echo json_encode(getRange($mysqli));
+    $mysqli->close();
+}
+
+function getAfterID($mysqli) {
+    $result = array();
     $id = $mysqli->real_escape_string($_REQUEST['after']);
     if (isset($_REQUEST['machine'])) {
         $machnum = $mysqli->real_escape_string($_REQUEST['machine']);
@@ -34,7 +43,9 @@ if (isset($_REQUEST['after'])) {
                 str_replace("-", "/", "$starttime UTC"),
                 str_replace("-", "/", "$stoptime UTC"));
         }
+        $rawdata->free();
     } else {
+        $machine_list_data = $mysqli->query("SELECT DISTINCT MACHNUM FROM CUT_CYCLE_TIMES ORDER BY MACHNUM");
         while ($machnum = $machine_list_data->fetch_assoc()) {
             $mach = $machnum['MACHNUM'];
             $s = "SELECT * FROM CUT_CYCLE_TIMES WHERE MACHNUM = '$mach' AND ID > $id";
@@ -52,9 +63,15 @@ if (isset($_REQUEST['after'])) {
                     $display_name, str_replace("-", "/", "$starttime UTC"),
                     str_replace("-", "/", "$stoptime UTC"));
             }
+            $rawdata->free();
+            $machine_list_data->free();
         }
     }
-} else {
+    return $result;
+}
+
+function getRange($mysqli) {
+    $result = array();
     $beg = date('Y-m-d\TH:i:s', strtotime($_REQUEST["start"]));
     $end = date('Y-m-d\TH:i:s', strtotime($_REQUEST["end"]));
     if (isset($_REQUEST['machine'])) {
@@ -76,7 +93,9 @@ if (isset($_REQUEST['after'])) {
                 str_replace("-", "/", "$starttime UTC"),
                 str_replace("-", "/", "$stoptime UTC"));
         }
+        $rawdata->free();
     } else {
+        $machine_list_data = $mysqli->query("SELECT DISTINCT MACHNUM FROM CUT_CYCLE_TIMES ORDER BY MACHNUM");
         while ($machnum = $machine_list_data->fetch_assoc()) {
             $mach = $machnum['MACHNUM'];
             $s = "SELECT * FROM CUT_CYCLE_TIMES WHERE MACHNUM = '$mach' AND STARTTIME > " .
@@ -96,10 +115,9 @@ if (isset($_REQUEST['after'])) {
                     str_replace("-", "/", "$stoptime UTC"));
             }
         }
+        $rawdata->free();
+        $machine_list_data->free();
     }
+    return $result;
 }
-$machine_list_data->free();
-$rawdata->free();
-$mysqli->close();
-echo json_encode($result);
 ?>
